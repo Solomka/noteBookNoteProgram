abacb1a5fb7d781aaaccdacfafa9a3d1ec282ccd
Solomka <sol.yaremko@gmail.com>
2017-04-25 07:01:23 +0300

    JUnit tests cors

--- src/test/java/ua/training/notebook_note/TestSuite.java
+++ src/test/java/ua/training/notebook_note/TestSuite.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ ModelTest.class, RegexContainerTest.class, NoteBookNoteBOConverterTest.class })
+@SuiteClasses({ ModelTest.class, RegexContainerTest.class })
 public class TestSuite {
 
 	/*
