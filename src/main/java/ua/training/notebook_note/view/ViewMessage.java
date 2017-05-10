269cfdb690523a1e02ee2bf1da2b04bc57a79e5d
Solomka <sol.yaremko@gmail.com>
2017-05-10 22:31:41 +0300

    strings modifier corrections

--- src/main/java/ua/training/notebook_note/view/ViewMessage.java
+++ src/main/java/ua/training/notebook_note/view/ViewMessage.java
@@ -8,59 +8,59 @@ package ua.training.notebook_note.view;
  */
 public interface ViewMessage {
 
-	public String TYPE = "Type ";
-	
-	public String NOTEBOOK_NOTE = "NOTEBOOK NOTE:";
+	String TYPE = "Type ";
 
-	public String FULLNAME = "FULLNAME:";
-	public String FULLNAME_FORMATTED = "FullName formatted:";
-	public String NAME = "Name";
-	public String SURNAME = "Surname";
-	public String PATRONYMIC = "Patronymic";
-	public String NICKNAME = "Nickname";
-	public String FULLNAME_LNGTH = " (3-35 symbols)";
+	String NOTEBOOK_NOTE = "NOTEBOOK NOTE:";
 
-	public String COMMENT = "Comment";
+	String FULLNAME = "FULLNAME:";
+	String FULLNAME_FORMATTED = "FullName formatted:";
+	String NAME = "Name";
+	String SURNAME = "Surname";
+	String PATRONYMIC = "Patronymic";
+	String NICKNAME = "Nickname";
+	String FULLNAME_LNGTH = " (3-35 symbols)";
 
-	public String CONTACTS = "CONTACTS:";
-	public String HOME_PHONE_NUM = "Home Number";
-	public String MOB_PHONE_NUM_1 = "Mobile Number1";
-	public String MOB_PHONE_NUM_2 = "Mobile Number2";
+	String COMMENT = "Comment";
 
-	public String HOME_PHONE_NUM_FORMAT = " (XXX-XX-XX)";
-	public String MOB_PHONE_NUM_FORMAT = " (+380-XX-XXX-XXXX)";
+	String CONTACTS = "CONTACTS:";
+	String HOME_PHONE_NUM = "Home Number";
+	String MOB_PHONE_NUM_1 = "Mobile Number1";
+	String MOB_PHONE_NUM_2 = "Mobile Number2";
 
-	public String EMAIL = "Email";
-	public String SKYPE = "Skype";
+	String HOME_PHONE_NUM_FORMAT = " (XXX-XX-XX)";
+	String MOB_PHONE_NUM_FORMAT = " (+380-XX-XXX-XXXX)";
 
-	public String GROUP = "Group";
-	public String GROUP_FORMAT = " (0 - SIMPLE CLIENT, 1 - VIP CLIENT)";
+	String EMAIL = "Email";
+	String SKYPE = "Skype";
 
-	public String ADDRESS = "ADDRESS:";
-	public String ADDRESS_FORMATTED = "Address Formatted:";
-	public String ADDRESS_CITY = "City";
-	public String ADDRESS_STREET = "Street";
-	public String ADDRESS_LNGTH = " (3-50 symbols)";
+	String GROUP = "Group";
+	String GROUP_FORMAT = " (0 - SIMPLE CLIENT, 1 - VIP CLIENT)";
 
-	public String ADDRESS_HOUSE_NUM = "House Number";
-	public String ADDRESS_APPART_NUM = "Appartment Number";
-	public String ADDRESS_INDEX = "Post Index";
-	
-	public String DATE_OF_CREATION = "Date of Creation:";
-	public String DATE_OF_UPDATE = "Date of Update:";
+	String ADDRESS = "ADDRESS:";
+	String ADDRESS_FORMATTED = "Address Formatted:";
+	String ADDRESS_CITY = "City";
+	String ADDRESS_STREET = "Street";
+	String ADDRESS_LNGTH = " (3-50 symbols)";
 
-	public String WRONG_INPUT = "Wrong input. Try again!";
+	String ADDRESS_HOUSE_NUM = "House Number";
+	String ADDRESS_APPART_NUM = "Appartment Number";
+	String ADDRESS_INDEX = "Post Index";
 
-	public String NICKNAME_EXCEPTION_MESSAGE = "Subsciber with further nickname already exists";
+	String DATE_OF_CREATION = "Date of Creation:";
+	String DATE_OF_UPDATE = "Date of Update:";
 
-	public String LEFT_PARENTHESIS = "[ ";
-	public String RIGHT_PARANTHESIS = " ]";
-	public String COMMA = ", ";
-	public String FULL_STOP = ".";
-	public String COLON = ": ";
-	public String EMPTY_STR = " ";
-	public String NEW_LINE = "\n";
-	public String BACKSLASH = "/";
-	public String SEMICOLON = ";";
+	String WRONG_INPUT = "Wrong input. Try again!";
+
+	String NICKNAME_EXCEPTION_MESSAGE = "Subsciber with further nickname already exists";
+
+	String LEFT_PARENTHESIS = "[ ";
+	String RIGHT_PARANTHESIS = " ]";
+	String COMMA = ", ";
+	String FULL_STOP = ".";
+	String COLON = ": ";
+	String EMPTY_STR = " ";
+	String NEW_LINE = "\n";
+	String BACKSLASH = "/";
+	String SEMICOLON = ";";
 
 }
